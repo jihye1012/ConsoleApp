dc787811b23af2ac57f048f832ba4a4a3a060b47
Jihye Yang <s2127@e-mirim.hs.kr>
2023-04-20 14:16:55 +0900

    지렁이 기어가다 if문

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,7 +11,28 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+
+                 if (x % 3 == 0)
+                 {
+                     Console.WriteLine("__@");
+                 }
+                 else if (x % 3 == 1)
+                 {
+                     Console.WriteLine("_^@");
+                 }
+                 else
+                 {
+                     Console.WriteLine("^_@");
+                 }
+                Thread.Sleep(1000);
+                x++;
+            }
         }
     }
 }
